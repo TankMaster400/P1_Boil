5ee988c3c9c235f5f1dd4dd534725b4805fbafa8
igor <igor@szumny.net>
2024-04-08 00:36:08 +0200

    Dane od Igora dla Franka

--- src/main/java/com/example/p1_boil/Controller.java
+++ src/main/java/com/example/p1_boil/Controller.java
@@ -259,29 +259,29 @@ public class Controller implements Initializable {
         stage.setScene(scene);
         stage.show();
 
-        DBConnect connection = new DBConnect();
-        Connection connectDB = connection.getDB();
-
-        String sqlS = "SELECT * FROM Records";
-
-        ObservableList<Records> list = FXCollections.observableArrayList();
-
-        try
-        {
-            Statement statement = connectDB.createStatement();
-            ResultSet resultSet = statement.executeQuery(sqlS);
-
-            while (resultSet.next())
-            {
-                list.add(new Records(resultSet.getString("Nazwa"),Integer.parseInt(resultSet.getString("Czas")),resultSet.getString("Po"),resultSet.getString("Przed"),Integer.parseInt(resultSet.getString("id"))));
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println(e);
-        }
+//        DBConnect connection = new DBConnect();
+//        Connection connectDB = connection.getDB();
+//
+//        String sqlS = "SELECT * FROM Records";
+//
+//        ObservableList<Records> list = FXCollections.observableArrayList();
+//
+//        try
+//        {
+//            Statement statement = connectDB.createStatement();
+//            ResultSet resultSet = statement.executeQuery(sqlS);
+//
+//            while (resultSet.next())
+//            {
+//                list.add(new Records(resultSet.getString("Nazwa"),Integer.parseInt(resultSet.getString("Czas")),resultSet.getString("Po"),resultSet.getString("Przed"),Integer.parseInt(resultSet.getString("id"))));
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//            System.out.println(e);
+//        }
         Data data = new Data();
-        //data.licz();
+        data.licz();
 
     }
 
