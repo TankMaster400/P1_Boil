0371450be622cb8b660eb77aeff950917ecb1a29
Franciszek Stolarczyk <franciszek.stolarczyk@gmail.com>
2024-04-22 02:12:01 +0200

    GUI Update

--- src/main/java/com/example/p1_boil/Controller.java
+++ src/main/java/com/example/p1_boil/Controller.java
@@ -9,10 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.Pane;
@@ -35,6 +32,8 @@ import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import static java.lang.Math.sqrt;
 
 public class Controller implements Initializable {
 
@@ -322,44 +321,107 @@ public class Controller implements Initializable {
         {
             System.out.println(e);
         }
-        //data.licz();
+        data.licz();
 
 
 
         //TODO: rewrite, one controller two fxml's - possibly wrong
-        Pane basePane = (Pane)fxmlLoader.getNamespace().get("genPane");
+        ScrollPane baseScrollPane = (ScrollPane)fxmlLoader.getNamespace().get("genPane");
+        Pane basePane = new Pane();
+        Integer sizeVariable = 90;
         //Iterate over nodes
         //TODO: rewrite, add classes for circles, do something with var i
+        System.out.println("Generowanie grafiki: START");
         Integer i = 1;
         ArrayList<Integer> tmp = new ArrayList<Integer>();
+        System.out.println("Generowanie: ilość node "+data.nodes.size());
         for(Node dataNode : data.nodes) {
-            Text text = new Text();
+            System.out.println("Generowanie: NODE "+dataNode.name);
             Circle circle = new Circle();
             circle.setStroke(Color.BLACK);
 
+            System.out.println("dataNode.sk: "+ dataNode.sk);
+            System.out.println("");
             if(dataNode.next.isEmpty()){
                 circle.setFill(Color.RED);
             }else if(dataNode.prev.isEmpty()){
                 circle.setFill(Color.GREEN);
+            }else if(dataNode.sk != null){
+                circle.setFill(Color.LIGHTBLUE);
             }else{
                 circle.setFill(null);
             }
 
-            circle.setRadius(10);
-            circle.setCenterX(5+30*i);
-            circle.setCenterY(5+30*i);
+            circle.setRadius(sizeVariable/3);
+            circle.setCenterX(sizeVariable*i);
+            circle.setCenterY(sizeVariable*i);
 
+            Text text = new Text();
             text.setFont(new Font(20));
             text.setWrappingWidth(200);
             text.setTextAlignment(TextAlignment.JUSTIFY);
             text.setText(dataNode.name);
             tmp.add(Integer.parseInt(dataNode.name));
-            text.setX(30*i);
-            text.setY(13+30*i);
+            text.setX(sizeVariable*i-5); //node number location
+            text.setY(-(sizeVariable/6)+4+sizeVariable*i); //offset to center
 
+            Text textB = new Text();
+            textB.setFont(new Font(15));
+            textB.setWrappingWidth(200);
+            textB.setTextAlignment(TextAlignment.JUSTIFY);
+            textB.setText(String.valueOf(dataNode.time_pk));
+            textB.setX(sizeVariable*i-sizeVariable/3.5); //node number location
+            textB.setY(sizeVariable*i+5); //offset to center
+
+            Text textC = new Text();
+            textC.setFont(new Font(15));
+            textC.setWrappingWidth(200);
+            textC.setTextAlignment(TextAlignment.JUSTIFY);
+            textC.setText(String.valueOf(dataNode.time_kp));
+            textC.setX(sizeVariable*i+sizeVariable/8); //node number location
+            textC.setY(sizeVariable*i+5); //offset to center
+
+            Text textD = new Text();
+            textD.setFont(new Font(15));
+            textD.setWrappingWidth(200);
+            textD.setTextAlignment(TextAlignment.JUSTIFY);
+            textD.setText(String.valueOf(dataNode.time_l));
+            textD.setX(sizeVariable*i-sizeVariable/18); //node number location
+            textD.setY(+(sizeVariable/6)+4+sizeVariable*i); //offset to center
+
+            Text textE = new Text();
+            textE.setFont(new Font(15));
+            textE.setWrappingWidth(2000);
+            textE.setTextAlignment(TextAlignment.JUSTIFY);
+            String finalString = "";
+            for(Czyn nextCzyn : dataNode.next){
+                finalString += "To: " + nextCzyn.next.name + " - Time: " + nextCzyn.time + " | ";
+            }
+            textE.setText(finalString);
+            textE.setX(sizeVariable*i+sizeVariable/2.5); //node number location
+            textE.setY(sizeVariable*i+5); //offset to center
+
+            Line crossA = new Line();
+            Line crossB = new Line();
+
+            crossA.setStartX((sqrt(2)/2)*(sizeVariable/3)+sizeVariable*i);
+            crossA.setStartY((sqrt(2)/2)*(sizeVariable/3)+sizeVariable*i);
+            crossA.setEndX((-(sqrt(2)/2)*(sizeVariable/3)+sizeVariable*i));
+            crossA.setEndY((-(sqrt(2)/2)*(sizeVariable/3)+sizeVariable*i));
+
+            crossB.setStartX(-(sqrt(2)/2)*(sizeVariable/3)+sizeVariable*i);
+            crossB.setStartY((sqrt(2)/2)*(sizeVariable/3)+sizeVariable*i);
+            crossB.setEndX(((sqrt(2)/2)*(sizeVariable/3)+sizeVariable*i));
+            crossB.setEndY((-(sqrt(2)/2)*(sizeVariable/3)+sizeVariable*i));
 
             basePane.getChildren().add(circle);
             basePane.getChildren().add(text);
+            basePane.getChildren().add(textB);
+            basePane.getChildren().add(textC);
+            basePane.getChildren().add(textD);
+            basePane.getChildren().add(textE);
+            basePane.getChildren().add(crossA);
+            basePane.getChildren().add(crossB);
             i++;
         }
         //Iterate over links
@@ -374,43 +436,44 @@ public class Controller implements Initializable {
             Integer prevNumberPosition = tmp.indexOf(Integer.parseInt(dataLink.prev.name))+1;
             Integer nextNumberPosition = tmp.indexOf(Integer.parseInt(dataLink.next.name))+1;
 
-            cubicCurve.setStartX(12+30*prevNumberPosition);
-            cubicCurve.setStartY(12+30*prevNumberPosition);
-
             if(switchOn){
-                cubicCurve.setControlX1(5+30*prevNumberPosition);
-                cubicCurve.setControlY1(40+30*prevNumberPosition);
-                cubicCurve.setControlX2(-40+30*nextNumberPosition);
-                cubicCurve.setControlY2(-5+30*nextNumberPosition);
+                cubicCurve.setControlX1(sizeVariable/8+sizeVariable*prevNumberPosition);
+                cubicCurve.setControlY1(sizeVariable+sizeVariable*prevNumberPosition);
+                cubicCurve.setControlX2(-sizeVariable+sizeVariable*nextNumberPosition);
+                cubicCurve.setControlY2(-sizeVariable/8+sizeVariable*nextNumberPosition);
                 switchOn = false;
             }else{
-                cubicCurve.setControlX1(40+30*prevNumberPosition);
-                cubicCurve.setControlY1(5+30*prevNumberPosition);
-                cubicCurve.setControlX2(-5+30*nextNumberPosition);
-                cubicCurve.setControlY2(-40+30*nextNumberPosition);
+                cubicCurve.setControlX1(sizeVariable+sizeVariable*prevNumberPosition);
+                cubicCurve.setControlY1(sizeVariable/8+sizeVariable*prevNumberPosition);
+                cubicCurve.setControlX2(-sizeVariable/8+sizeVariable*nextNumberPosition);
+                cubicCurve.setControlY2(-sizeVariable+sizeVariable*nextNumberPosition);
                 switchOn = true;
             }
 
-            cubicCurve.setEndX(-5+30*nextNumberPosition);
-            cubicCurve.setEndY(5+30*nextNumberPosition);
+            cubicCurve.setStartX(sizeVariable/3+sizeVariable*prevNumberPosition);
+            cubicCurve.setStartY(sizeVariable*prevNumberPosition);
 
-            arrowPartA.setStartX(-10+30*nextNumberPosition);
-            arrowPartA.setStartY(10+30*nextNumberPosition);
-            arrowPartB.setStartX(-10+30*nextNumberPosition);
-            arrowPartB.setStartY(0+30*nextNumberPosition);
+            cubicCurve.setEndX(-sizeVariable/3+sizeVariable*nextNumberPosition);
+            cubicCurve.setEndY(sizeVariable*nextNumberPosition);
 
-            arrowPartA.setEndX(-5+30*nextNumberPosition);
-            arrowPartA.setEndY(5+30*nextNumberPosition);
-            arrowPartB.setEndX(-5+30*nextNumberPosition);
-            arrowPartB.setEndY(5+30*nextNumberPosition);
+            Integer arrowOffset = -sizeVariable/6;
+            arrowPartA.setStartX(arrowOffset+(-sizeVariable/3)+sizeVariable*nextNumberPosition);
+            arrowPartA.setStartY(-arrowOffset+sizeVariable*nextNumberPosition);
+            arrowPartB.setStartX(arrowOffset+(-sizeVariable/3)+sizeVariable*nextNumberPosition);
+            arrowPartB.setStartY(arrowOffset+sizeVariable*nextNumberPosition);
+
+            arrowPartA.setEndX(-sizeVariable/3+sizeVariable*nextNumberPosition);
+            arrowPartA.setEndY(sizeVariable*nextNumberPosition);
+            arrowPartB.setEndX(-sizeVariable/3+sizeVariable*nextNumberPosition);
+            arrowPartB.setEndY(sizeVariable*nextNumberPosition);
 
             cubicCurve.setStroke(Color.FORESTGREEN);
             cubicCurve.setStrokeWidth(1);
 
-            arrowPartA.setStroke(Color.FORESTGREEN);
+            arrowPartA.setStroke(Color.BLACK);
             arrowPartA.setStrokeWidth(1);
 
-            arrowPartB.setStroke(Color.FORESTGREEN);
+            arrowPartB.setStroke(Color.BLACK);
             arrowPartB.setStrokeWidth(1);
 
             cubicCurve.setFill(null);
@@ -418,8 +481,7 @@ public class Controller implements Initializable {
             basePane.getChildren().add(arrowPartA);
             basePane.getChildren().add(arrowPartB);
         }
-        data.licz();
-
+        baseScrollPane.setContent(basePane);
     }
 
 
